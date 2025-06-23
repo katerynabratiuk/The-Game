16ccd269e48c19e24eb1a1817763e4f49a32c01b
Kateryna Bratiuk <kate.bratiuk@gmail.com>
2025-06-23 17:29:41 +0300

    add type field to Item

--- GameServer/src/main/java/org/server/db/model/Item.java
+++ GameServer/src/main/java/org/server/db/model/Item.java
@@ -3,9 +3,21 @@ package org.server.db.model;
 import lombok.Getter;
 import lombok.Setter;
 
+
+
 @Getter
 @Setter
 public abstract class Item {
+    private final ItemType type;
     private Integer id;
     private String name;
+
+    public Item(ItemType itemType) {
+        type = itemType;
+    }
+
+    public enum ItemType {
+        GUN, HEAL, FLASH, SPEED
+    }
+
 }
