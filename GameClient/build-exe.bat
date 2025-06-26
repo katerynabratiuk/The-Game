835eb44f5418ca6c8d7a6ce6c070605618ee1703
stasiaaleks <anastasiia@newtonideas.com>
2025-06-26 12:46:02 +0300

    update build-exe.bat

--- GameClient/build-exe.bat
+++ GameClient/build-exe.bat
@@ -3,6 +3,6 @@ jpackage ^
   --input target/ ^
   --name Game-Executable ^
   --main-jar game-client.jar ^
-  --main-class org.client.UDPClient ^
+  --main-class org.client.GameClient ^
   --type app-image ^
   --win-console
